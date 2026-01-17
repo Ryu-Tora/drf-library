3e00ae6732b14632a7ae571584a90bd5b4f51295
Adam Halka <galkaadam4@gmail.com>
2026-01-17 19:01:42 +0100

    add borrow views

--- borrow/views.py
+++ borrow/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrow.models import Borrowing
+from borrow.serializers import BorrowingSerializer, BorrowingDetailSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.select_related()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingSerializer
