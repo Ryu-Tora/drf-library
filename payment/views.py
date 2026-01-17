98cf366f7409e30b5859092954f91ea00adaf462
Adam Halka <galkaadam4@gmail.com>
2026-01-17 19:45:50 +0100

    add payment views

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from payment.models import Payment
+from payment.permissions import IsOwnerOrAdmin
+from payment.serializers import PaymentDetailSerializer, PaymentSerializer
+
+
+class PaymentViewSet(ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        queryset = Payment.objects.select_related(
+            "borrowing", "borrowing__user"
+        )
+        if self.request.user.is_staff:
+            return queryset
+
+        return queryset.filter(borrowing__user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+        return PaymentSerializer
